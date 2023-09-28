6c66748868955517182eac2a5216e313829a72ea
Sofiia Ivanko <sivanko@man.poznan.pl>
2023-09-28 03:44:46 +0200

    Add shots to output

--- atos_stuff/algorithms.py
+++ atos_stuff/algorithms.py
@@ -1,4 +1,4 @@
-""" file with orca algorithms subclasses """
+''' file with orca algorithms subclasses '''
 
 from qat.plugins import ScipyMinimizePlugin
 from qat.qpus import get_default_qpu
@@ -9,36 +9,35 @@ from templates import Problem, Algorithm
 
 
 class QAOA2(Algorithm):
-    """ Algorithm class with QAOA """
+    ''' Algorithm class with QAOA '''
 
     def __init__(self, p: int = 1, aux=None):
         self.name = 'qaoa'
+        self.path_name = f'{self.name}@{p}'
         self.aux = aux
         self.p: int = p
         self.parameters = ['p']
-        self._set_path()
-
-    def _set_path(self) -> None:
-        self.path = f'{self.name}@{self.p}'
 
     def run(self, problem: Problem, backend: AtosBackend) -> dict:
-
-        """ Runs the QAOA algorithm """
+        
+        ''' Runs the QAOA algorithm '''
         observable = self.get_problem_data(problem)
-
         circuit = AnsatzFactory.qaoa_circuit(observable, self.p, strategy='default')
-
-        job = circuit.to_job(observable=observable)
         qpu = get_default_qpu()
         stack = ScipyMinimizePlugin(method="COBYLA",
-                                    tol=1e-5,
+                                    tol=1e-5, 
                                     options={"maxiter": 200}) | qpu
-        result = stack.submit(job)
-
-        return result
+                                    
+        job = circuit.to_job(observable=observable)
+        result = qpu.submit(job)
+        sjob = job(**eval(result.meta_data["parameter_map"]))
+        sjob = sjob.circuit.to_job(nbshots=1024)
+        sresult = qpu.submit(sjob)
+        
+        return sresult
 
     def check_problem(self, problem: Problem) -> bool:
-        """ Check if the problem implements get_hamiltonian method"""
+        ''' Check if the problem implements get_hamiltonian method'''
         return callable(getattr(problem, 'get_atos_hamiltonian', False))
 
     def get_problem_data(self, problem: Problem):
