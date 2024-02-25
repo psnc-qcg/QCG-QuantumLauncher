3faa0d7a8a8545f6d34ef3199c8c9b9ba3c2bb3f
Dawid64 <dawid.siera@gmail.com>
2024-02-25 15:52:39 +0100

    Orca Binary Bosonic parametrized

--- orca_routines/algorithms.py
+++ orca_routines/algorithms.py
@@ -1,6 +1,7 @@
 """ file with orca algorithms subclasses """
 
 from ptseries.algorithms.binary_solvers import BinaryBosonicSolver
+from ptseries.common.logger import Logger
 
 from templates import Problem, Algorithm
 from .backend import OrcaBackend
@@ -10,22 +11,29 @@ from .orca_templates import OrcaRoutine
 class BinaryBosonic(Algorithm, OrcaRoutine):
     """ Orca Algorithm """
 
-    def __init__(self) -> None:
+    def __init__(self, learning_rate:float=1e-1, updates:int=80, tbi_loops:str='single-loop') -> None:
         super().__init__()
         self.bbs = None
+        self.learning_rate = learning_rate
+        self.updates = updates
+        self.tbi_loops = tbi_loops
+        self.logger = Logger(log_dir=None)
 
     def _get_path(self) -> str:
         return 'BinaryBosonic'
 
     def run(self, problem: Problem, backend: OrcaBackend):
         qubo_fn_fact, Q = problem.get_orca_qubo()
-        self.bbs = BinaryBosonicSolver(len(Q),
-                                       qubo_fn_fact(Q)
-                                       )
+        self.bbs = BinaryBosonicSolver(
+            len(Q),
+            qubo_fn_fact(Q),
+            tbi_params={"tbi_type": self.tbi_loops}
+        )
         self.bbs.train(
-            learning_rate=1e-1,
-            updates=80,
-            print_frequency=20
+            learning_rate=self.learning_rate,
+            updates=self.updates,
+            print_frequency=20,
+            logger=self.logger
         )
 
         return self.bbs.return_solution()
