da90337637ae19e8315878d0e47cbbb90569e4be
Dawid Siera <dsiera@man.poznan.pl>
2024-04-25 08:46:53 +0200

    added qubo_into_hamiltonian function

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from qat.core import Observable, Term
 from qiskit.quantum_info import SparsePauliOp
+from typing import Iterable
 
 
 def ham_from_qiskit_to_atos(q_h: SparsePauliOp) -> Observable:
@@ -8,3 +9,37 @@ def ham_from_qiskit_to_atos(q_h: SparsePauliOp) -> Observable:
     for term in q_h.to_list():
         line_obs.add_term(Term(term[1].real, term[0], qubit_list))
     return line_obs
+
+
+def qubo_into_hamiltonian(qubo: Iterable[Iterable[int]], bias: float = 0) -> SparsePauliOp:
+    """_summary_
+
+    Args:
+        qubo (Iterable[Iterable[int]]): Quadratic Unconstrained Binary Optimization Matrix.
+        bias (float, optional): The bias value that will be added to identity. Defaults to 0.
+
+    Returns:
+        SparsePauliOp: The quadratic Hamiltonian in the form of a SparsePauliOp.
+    """
+    N = len(qubo)
+
+    def _create_string(z_index: int | list[int] | None) -> str:
+        if z_index is None:
+            return 'I' * N
+
+        if isinstance(z_index, int):
+            z_index = [z_index]
+
+        string = ''.join('I' if i not in z_index else 'Z' for i in range(N))
+        return string
+
+    hamiltonian = SparsePauliOp(_create_string(None), bias)
+    for ind_r, row in enumerate(qubo):
+        for ind_c, val in enumerate(row[:ind_r + 1]):
+            if ind_c == ind_r:
+                hamiltonian += SparsePauliOp(_create_string(ind_c), val)
+                continue
+            if val != 0:
+                hamiltonian += SparsePauliOp(
+                    _create_string([ind_c, ind_r]), val)
+    return hamiltonian
