e5b52a6628981d8804aaa1e29183272bb075337e
Dawid Siera <dsiera@man.poznan.pl>
2024-05-10 16:32:44 +0200

    added alternative hamiltonian to qubo convertion

--- dwave_routines/basic_problems.py
+++ dwave_routines/basic_problems.py
@@ -8,35 +8,19 @@ from qiskit_routines.basic_problems import (
 )
 from .dwave_templates import DwaveRoutine
 from qiskit.quantum_info import SparsePauliOp
+from qiskit_optimization.converters import QuadraticProgramToQubo
+from qiskit_optimization.translators import from_ising
 
 
 class QiskitToDwave:
     def get_qubo(self):
         return self._hamiltonian_to_qubo(self.get_qiskit_hamiltonian())
 
-    def _hamiltonian_to_qubo(self, values: SparsePauliOp):
-        """
-        Function to convert Z matrix to QUBO matrix
-        """
-        max_qubit = values.num_qubits
-        # print(max_qubit)
-        qubo = np.zeros((max_qubit, max_qubit))
-        for value in values:
-            pauli = value.paulis[0]
-            coeff: np.complex128 = value.coeffs[0]
-            target_qubits = [ind for ind, x in enumerate(
-                str(pauli)) if x == 'Z']
-            pauli_type = str(value.paulis[0]).replace('I', '')
-            if pauli_type != '':
-                if pauli_type == 'ZZ':
-                    qubo[target_qubits[0], target_qubits[1]] = coeff.real
-                    qubo[target_qubits[1], target_qubits[0]] = coeff.real
-                elif pauli_type == 'Z':
-                    qubo[target_qubits[0], target_qubits[0]] = coeff.real
-
-            else:
-                offset = coeff.real
-        return qubo, offset
+    def _hamiltonian_to_qubo(self, hamiltonian: SparsePauliOp) -> tuple[np.ndarray, float]:
+        qp = from_ising(hamiltonian)
+        conv = QuadraticProgramToQubo()
+        qubo = conv.convert(qp).objective
+        return qubo.quadratic.to_array(), qubo.constant
 
 
 class ECDwave(ECQiskit, EC, QiskitToDwave, DwaveRoutine):
