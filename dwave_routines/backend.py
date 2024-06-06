b6ad982d2aee3d0f1344c156c59ac1523e09c0fd
Dawid Siera <dsiera@man.poznan.pl>
2024-06-06 22:53:44 +0200

    Added new Dwave Backend

--- dwave_routines/backend.py
+++ dwave_routines/backend.py
@@ -2,15 +2,22 @@ from templates import Backend
 from .dwave_templates import DwaveRoutine
 from tabu import TabuSampler
 from dwave.system import DWaveSampler, EmbeddingComposite
+from dwave.samplers import SimulatedAnnealingSampler
 
 
 class TabuBackend(Backend, DwaveRoutine):
-    def __init__(self, name: str = "tabu", parameters: list = None) -> None:
+    def __init__(self, name: str = "TabuSampler", parameters: list = None) -> None:
         super().__init__(name, parameters)
         self.sampler = TabuSampler()
 
 
 class DwaveBackend(Backend, DwaveRoutine):
-    def __init__(self, name: str = "DwaveSampler", parameters: list = None) -> None:
+    def __init__(self, name: str = "DWaveSampler", parameters: list = None) -> None:
         super().__init__(name, parameters)
         self.sampler = EmbeddingComposite(DWaveSampler())
+
+
+class DwaveFakeBackend(Backend, DwaveRoutine):
+    def __init__(self, name: str = "SimulatedAnnealingSampler", parameters: list = None) -> None:
+        super().__init__(name, parameters)
+        self.sampler = SimulatedAnnealingSampler()
