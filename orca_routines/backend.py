8d3395108ce025f7ee0b069d4cd7b1769777e73d
Dawid Siera <dsiera@man.poznan.pl>
2024-06-07 11:01:55 +0200

    Add optional system backend

--- orca_routines/backend.py
+++ orca_routines/backend.py
@@ -1,6 +1,7 @@
 """ Backend class for Orca """
 from templates import Backend
 from .orca_templates import OrcaRoutine
+from typing import Literal
 
 
 class OrcaBackend(Backend, OrcaRoutine):
@@ -9,15 +10,20 @@ class OrcaBackend(Backend, OrcaRoutine):
     def __init__(self, name: str) -> None:
         super().__init__(name)
 
-    def get_args():
+    def get_args(self):
         return {}
 
 
 class PCSSOrcaBackend(Backend, OrcaRoutine):
     """ Orca QPU backend """
 
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: Literal['system-A', 'system-B']) -> None:
         super().__init__(name)
+        self.system_name = name
 
-    def get_args():
-        return {"tbi_type": "PT-1", "ip_address": "169.254.109.10"}
+    def get_args(self):
+        if self.system_name == 'system-A':
+            return {"tbi_type": "PT-1", "ip_address": "169.254.109.10"}
+        if self.system_name == 'system-B':
+            return {"tbi_type": "PT-2", "ip_address": "0.0.0.0"}
+        raise ValueError(f"Unknown backend {self.system_name}")
