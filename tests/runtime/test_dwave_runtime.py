26fe94e5b9602b40f681868fec38b97a636a0c32
Dawid Siera <dsiera@man.poznan.pl>
2024-06-07 12:42:33 +0200

    Add runtime tests

---
+++ tests/runtime/test_dwave_runtime.py
@@ -0,0 +1,49 @@
+from templates import QuantumLauncher
+from dwave_routines import DwaveSolver, SimulatedAnnealingBackend
+from problems import EC, JSSP, MaxCut, QATM
+TESTING_DIR = 'testing'
+
+
+def test_ec():
+    """ Testing function for Exact Cover """
+    pr = EC('quadratic')
+    pr.set_instance(instance_name='toy', instance=None)
+    solver = DwaveSolver(1)
+    backend = SimulatedAnnealingBackend()
+    launcher = QuantumLauncher(pr, solver, backend, path=TESTING_DIR)
+
+    inform = launcher._run()
+    assert inform is not None
+
+
+def test_jssp():
+    """ Testing function for Job Shop Shedueling Problem """
+    pr = JSSP(3, 'quadratic', instance_name='toy', optimization_problem=True)
+    solver = DwaveSolver(1)
+    backend = SimulatedAnnealingBackend()
+    launcher = QuantumLauncher(pr, solver, backend, path=TESTING_DIR)
+
+    inform = launcher._run()
+    assert inform is not None
+
+
+def test_maxcut():
+    """ Testing function for Max Cut """
+    pr = MaxCut(instance_name='default')
+    solver = DwaveSolver(1)
+    backend = SimulatedAnnealingBackend()
+    launcher = QuantumLauncher(pr, solver, backend, path=TESTING_DIR)
+
+    inform = launcher._run()
+    assert inform is not None
+
+
+def test_qatm():
+    """ Testing function for Max Cut """
+    pr = QATM('exact', instance_name='RCP_3.txt', instance_path='data/qatm/')
+    solver = DwaveSolver(1)
+    backend = SimulatedAnnealingBackend()
+    launcher = QuantumLauncher(pr, solver, backend, path=TESTING_DIR)
+
+    inform = launcher._run()
+    assert inform is not None
---
+++ tests/runtime/test_orca_runtime.py
@@ -0,0 +1,38 @@
+from templates import QuantumLauncher
+from orca_routines import BBS, OrcaBackend
+from problems import EC, JSSP, MaxCut, QATM
+TESTING_DIR = 'testing'
+
+
+def test_ec():
+    """ Testing function for Exact Cover """
+    pr = EC('exact')
+    pr.set_instance(instance_name='toy', instance=None)
+    bbs = BBS()
+    backend = OrcaBackend('local_simulator')
+    launcher = QuantumLauncher(pr, bbs, backend, path=TESTING_DIR)
+
+    inform = launcher._run()
+    assert inform is not None
+
+
+def test_jssp():
+    """ Testing function for Job Shop Shedueling Problem """
+    pr = JSSP(3, 'exact', instance_name='toy', optimization_problem=True)
+    bbs = BBS()
+    backend = OrcaBackend('local_simulator')
+    launcher = QuantumLauncher(pr, bbs, backend, path=TESTING_DIR)
+
+    inform = launcher._run()
+    assert inform is not None
+
+
+def test_maxcut():
+    """ Testing function for Max Cut """
+    pr = MaxCut(instance_name='default')
+    bbs = BBS()
+    backend = OrcaBackend('local_simulator')
+    launcher = QuantumLauncher(pr, bbs, backend, path=TESTING_DIR)
+
+    inform = launcher._run()
+    assert inform is not None
