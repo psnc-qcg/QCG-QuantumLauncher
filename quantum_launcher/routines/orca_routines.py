096a9d3305d9450b570b65b656547d614661f7ab
Dawid64 <dawid.siera@gmail.com>
2025-01-02 15:29:15 +0100

    Changed orca_routines into single file

--- quantum_launcher/routines/orca_routines.py
+++ quantum_launcher/routines/orca_routines.py
@@ -3,10 +3,19 @@ from typing import List, Literal
 from ptseries.algorithms.binary_solvers import BinaryBosonicSolver
 from ptseries.common.logger import Logger
 
-from quantum_launcher.base import Problem, Algorithm, Result
-from .backend import OrcaBackend
+from quantum_launcher.base import Problem, Algorithm, Result, Backend
 from typing import Callable
 import numpy as np
+
+
+class OrcaBackend(Backend):
+    """ local backend """
+
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def get_args(self):
+        return {}
 
 
 class BBS(Algorithm):
@@ -32,12 +41,11 @@ class BBS(Algorithm):
     """
     _algorithm_format = 'qubo'
 
-    def __init__(self, format:Literal['qubo', 'qubo_fn'] = 'qubo', **kwargs) -> None:
+    def __init__(self, format: Literal['qubo', 'qubo_fn'] = 'qubo', **kwargs) -> None:
         super().__init__()
         self._algorithm_format = format
         self.kwargs = kwargs
         self.input_state = kwargs.get('input_state', None)
-
 
     def run(self, problem: Problem, backend: OrcaBackend, formatter: Callable[[Problem], np.ndarray]):
         # params = {"tbi_type": self.kwarga['tbi_type']}
@@ -64,7 +72,8 @@ class BBS(Algorithm):
 
     def construct_results(self, results: BinaryBosonicSolver) -> Result:
         # TODO: add support for distribution (probably with different logger)
-        best_bitstring = ''.join(map(str, map(int, results.config_min_encountered)))
+        best_bitstring = ''.join(
+            map(str, map(int, results.config_min_encountered)))
         best_energy = results.E_min_encountered
         most_common_bitstring = None
         most_common_bitstring_energy = None
@@ -76,31 +85,3 @@ class BBS(Algorithm):
         return Result(best_bitstring, best_energy, most_common_bitstring,
                       most_common_bitstring_energy, distribution, energy,
                       num_of_samples, average_energy, energy_std, results)
-
-
-class BBSFN(BBS):
-    def run(self, problem: Problem, backend: OrcaBackend, formatter: Callable[[Problem], Callable]):
-        params = {"tbi_type": self.tbi_loops}
-        if backend is not None:
-            params.update(backend.get_args())
-        qubo_fn_fact = formatter(problem)
-        Q = None
-        # TODO make this thing working
-        if self.input_state is None:
-            self.input_state = [not i % 2 for i in range(len(Q))]
-        self.bbs = BinaryBosonicSolver(
-            pb_dim=len(Q),
-            objective=Q,
-            gradient_mode=self.gradient_mode,
-            tbi_params=params,
-            n_samples=self.n_samples,
-            input_state=self.input_state
-        )
-        self.bbs.train(
-            learning_rate=self.learning_rate,
-            updates=self.updates,
-            log_frequency=self.print_frequency,
-            logger=self.logger
-        )
-
-        return self.bbs.config_min_encountered
--- quantum_launcher/routines/orca_routines/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-``orca_routines``
-==============
-
-The Quantum Launcher version made for Orca
-"""
-from .algorithms import BBS
-from .backend import OrcaBackend
